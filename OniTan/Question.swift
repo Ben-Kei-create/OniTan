281b4fdd4b0a113b8ca017f96e6f6ec7c51e49e4
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-02-19 23:30:01 +0900

    Fix Question decoding by generating id locally

--- OniTan/Question.swift
+++ OniTan/Question.swift
@@ -1,21 +1,15 @@
 import Foundation
 
 struct Question: Identifiable, Codable {
-    let id: UUID
+    /// JSONには無いのでデコード対象外。アプリ側で生成する
+    let id: UUID = UUID()
+
     let kanji: String
     let choices: [String]
     let answer: String
     let explain: String
 
-    enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case kanji, choices, answer, explain
-    }
-
-    init(kanji: String, choices: [String], answer: String, explain: String) {
-        self.id = UUID()
-        self.kanji = kanji
-        self.choices = choices
-        self.answer = answer
-        self.explain = explain
     }
 }
